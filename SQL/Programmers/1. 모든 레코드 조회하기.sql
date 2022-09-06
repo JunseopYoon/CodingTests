5807e5c71297ac8272ea6cae3a8da473b173815e
Junseop Yoon <junseopyoon45@gmail.com>
2022-09-07 00:06:22 +0900

    Oracle coding tests

---
+++ SQL/Programmers/1. 모든 레코드 조회하기.sql
@@ -0,0 +1,6 @@
+SELECT *
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+--출처: 프로그래머스 코딩 테스트 연습
+--https://school.programmers.co.kr/learn/challenges
---
+++ SQL/Programmers/3. 아픈 동물 찾기.sql
@@ -0,0 +1,7 @@
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID
+
+--출처: 프로그래머스 코딩 테스트 연습
+--https://school.programmers.co.kr/learn/challenges
