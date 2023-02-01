ed2b7278879986e9967b4817bf7675ba864a8cd2
Junseop Yoon <96647847+JunseopYoon@users.noreply.github.com>
2023-02-01 22:39:23 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,14 @@
+h, m = map(int, input().split())
+t = int(input())
+hour = (m+t) // 60
+min = (m + t)%60
+
+if (m+t >= 60):
+    if(h+hour >= 24):
+        h = h-24
+    h = h + hour
+    print(h, min)
+else:
+    if(h >= 24):
+        h = h-24
+    print(h, m+t)
